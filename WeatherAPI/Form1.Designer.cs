130d6a058857b10d2b3b60bf19b0fb2568fa08c1
Pallavi12344 <Pallavi.s.khobanna@gmail.com>
2025-08-07 01:21:17 +0200

    initial commit My Winform WeatherAPI

---
+++ WeatherAPI/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿
+namespace WeatherAPI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblCondition = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCity
+            // 
+            this.txtCity.BackColor = System.Drawing.SystemColors.Info;
+            this.txtCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCity.Location = new System.Drawing.Point(54, 114);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(235, 34);
+            this.txtCity.TabIndex = 0;
+            this.txtCity.TextChanged += new System.EventHandler(this.txtCity_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(382, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Getweather";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemp.Location = new System.Drawing.Point(62, 257);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(159, 29);
+            this.lblTemp.TabIndex = 2;
+            this.lblTemp.Text = "Temperature:";
+            this.lblTemp.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lblCondition
+            // 
+            this.lblCondition.AutoSize = true;
+            this.lblCondition.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCondition.Location = new System.Drawing.Point(62, 312);
+            this.lblCondition.Name = "lblCondition";
+            this.lblCondition.Size = new System.Drawing.Size(122, 29);
+            this.lblCondition.TabIndex = 3;
+            this.lblCondition.Text = "Condition:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(49, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Enter City ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(583, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(152, 109);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblCondition);
+            this.Controls.Add(this.lblTemp);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtCity);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label lblCondition;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WeatherAPI/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace WeatherAPI
+{
+    public partial class Form1 : Form
+    {
+        private string API_KEY = "cc86c964d8c97c85b249209f123d5f91";
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string LastCity = Properties.Settings.Default.LastCity;
+            if (!string.IsNullOrEmpty(LastCity))
+            {
+                txtCity.Text = LastCity;
+            }
+        }
+
+       
+
+        private async Task GetWeatherAsync(string city)
+        {
+            string URL = $"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric";
+
+            using (HttpClient client= new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(URL) ;
+                    if(!response.IsSuccessStatusCode)
+                    {
+                        if(response.StatusCode==System.Net.HttpStatusCode.NotFound)
+                        {
+                            MessageBox.Show("City not found.Please check the city name.", 
+                                    "Invalid City", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Error : {response.StatusCode}", "Weather API Error",
+                                MessageBoxButtons.OK,MessageBoxIcon.Error);
+                        }
+                        return;
+                    }
+
+                    string json = await client.GetStringAsync(URL);
+
+                    JObject data = JObject.Parse(json);
+                    double temp = (double)data["main"]["temp"];
+                    string condition = data["weather"][0]["description"].ToString();
+                    string IconCode =data["weather"][0]["icon"].ToString();
+
+                    lblTemp.Text = $"Temperature: {temp} °C";
+                    lblCondition.Text = $"Condition: {condition}";
+                    pictureBox1.Load($"https://openweathermap.org/img/wn/{IconCode}@2x.png");
+
+                }
+                catch (HttpRequestException)
+                {
+                    MessageBox.Show("Network error! Check your internet connection.",
+                                    "Connection Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error fetching weather: " + ex.Message);
+                }
+            }
+        }
+        private void txtCity_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            string city = txtCity.Text.Trim();
+            if (string.IsNullOrEmpty(city))
+            {
+                MessageBox.Show("please enter a city name.","Input Required",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+                return;
+            }
+
+            Properties.Settings.Default.LastCity = city;
+            Properties.Settings.Default.Save();
+            await GetWeatherAsync(city);
+        }
+    }
+}
